6dc2c9a18c249e93ebfb765faf68716aedc47bbc
imclr <162337823+imclr@users.noreply.github.com>
2024-03-05 22:50:38 -0500

    ui

---
+++ app/components/math.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import renderMathInElement from 'katex/dist/contrib/auto-render';
+import 'katex/dist/katex.min.css';
+import { useEffect, useRef } from 'react';
+
+export default function Math({ text, ...delegated }: any) {
+
+  const MathRef = useRef();
+
+  useEffect(() => {
+    if (MathRef.current) {
+      renderMathInElement(MathRef.current, {
+        delimiters: [
+          { left: '$$', right: '$$', display: true },
+          { left: '$', right: '$', display: false },
+        ],
+      });
+    }
+  }, [text]);
+
+  return (
+    <div ref={MathRef} {...delegated}>
+      {text}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Math from './components/math';
+
 export default function Home() {
-    return <h1>Directed Reading Program 2024 Spring</h1>
+    return <h1>Directed Reading Program 2024 Spring <Math text={'$$x^2$$'} /></h1>
 }
